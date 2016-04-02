7f6286b2985274e9460794401117487b62b4bf24
Johannes Hansen <johannesblhansen@gmail.com>
2016-04-02 20:00:42 +0200

    Got the static resources to work

--- assignment/src/main/java/com/teamonline/configuration/SecurityConfiguration.java
+++ assignment/src/main/java/com/teamonline/configuration/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home", "/register", "/createAppUser", "/userCreated").permitAll().anyRequest().authenticated().and().formLogin()
+		http.authorizeRequests().antMatchers("/", "/home", "/register", "/createAppUser", "/userCreated", "/css/*").permitAll().anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
