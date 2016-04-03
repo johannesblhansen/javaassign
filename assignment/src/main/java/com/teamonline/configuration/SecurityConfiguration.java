878a44a44786bf360c390a51c4a14af34cd64ac0
Johannes Hansen <johannesblhansen@gmail.com>
2016-04-03 16:45:55 +0200

    Formatting and xhtml

--- assignment/src/main/java/com/teamonline/configuration/SecurityConfiguration.java
+++ assignment/src/main/java/com/teamonline/configuration/SecurityConfiguration.java
@@ -41,8 +41,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home", "/register", "/createAppUser", "/userCreated", "/css/*", "/js/*", "/rest/**").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+		http.authorizeRequests().antMatchers(
+											"/", 
+											"/home", 
+											"/register", 
+											"/createAppUser", 
+											"/userCreated", 
+											"/css/*", 
+											"/js/*", 
+											"/rest/**")
+												.permitAll().anyRequest().authenticated().and()
+												.formLogin().loginPage("/login").permitAll().and()
+												.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+
 	}
 
 	/**
